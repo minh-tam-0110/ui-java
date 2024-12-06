142f9ae12f6fab907af4d1e2622a2ba0207d5e9b
Minh Tam <htam0110@gmail.com>
2024-12-06 20:45:00 +0700

    Revert "[ID-18] update form success"
    This reverts commit 31bb5cff6a23a366db929d21fe9debe47574d66a.

--- src/test/java/testcase/form/FormTest.java
+++ src/test/java/testcase/form/FormTest.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FormTest extends MasterTest {
